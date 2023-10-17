938828794a239ee818c37b82e25ea4b66f4b2691
saysa <saysaygood83@gmail.com>
2023-10-17 16:53:27 +0100

    added inputs from questions abt manager using inquirer

--- index.js
+++ index.js
@@ -13,3 +13,49 @@ const render = require("./src/page-template.js");
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
+const teamMembers = []
+const idList = []
+
+const  appMenu = () => {
+    function createManager(){
+        console.log("Please build you team!");
+        inquirer.prompt([
+            {
+                type: "input",
+                name: 'managerName',
+                message: "What is the team manager's name?",
+                validate: answer => {
+                    if(answer != ""){
+                        return true
+                    }
+                    return "Please enter at least one character."
+                }
+            },
+            {
+                type: "input",
+                name: "managerID",
+                message: "What is the team manager's id?"
+            },
+            {
+                type: "input",
+                name: "managerEmail",
+                message: "What is the team manager's office number?"
+            },
+            {
+                type: "input",
+                name: "managerOfficeNumber",
+                message: "What is the team manager's office number"
+            },
+        ]).then(answer => {
+            const manager = new Manager(answer.managerName, answer.managerId, answer.managerEmail, answer.managerOfficeNumber)
+            teamMembers.push(manager);
+            idList.push(answer.managerId);
+            createTeam();
+        })
+    }
+
+    createManager();
+}
+
+
+appMenu()
